04c496de21e698d70ec3f80e0fe1941e5678ebf9
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-04-16 06:47:39 +0300

    2

--- js/task-09.js
+++ js/task-09.js
@@ -1,25 +1,19 @@
-const colorPallete = document.querySelector(".widget");
-const colorBody = document.querySelector("body");
-const colorSpan = document.querySelector(".color");
-
-const onClick = (e) => {
-  if (e.target.nodeName !== "BUTTON") {
-    return;
-  }
-  console.log("click");
-  console.log(e.target.dataset.color);
-  colorSpan.textContent = e.target.dataset.color;
- 
-}
-colorBody.addEventListener("click", onClick);
-const color = document.querySelector("button")
-const getHexColor = getRandomHexColor();
-color.dataset.color = getHexColor;
-colorBody.style.backgroundColor = getHexColor;
-
 function getRandomHexColor() {
-
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorChangeButton = document.querySelector(".change-color");
+const colorBody = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+colorChangeButton.addEventListener('click', () => {
+
+colorBody.style.backgroundColor = getRandomHexColor();
+colorSpan.textContent = getRandomHexColor();
+});
+
+
+
+
